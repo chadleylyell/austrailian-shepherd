3c498fc6e248256be8d44ee60088aac4505d7ddf
Chadley Lyell <chadley.lyell@bartlettpanthers.org>
2016-11-02 11:35:34 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            $('nav a[href*=#]').click(function () {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+               && location.hostname == this.hostname) {
+                    var $target = $(this.hash);
+                    $target = $target.length && $target
+                    || $('[name=' + this.hash.slice(1) + ']');
+                    if ($target.length) {
+                        var targetOffset = $target.offset().top;
+                        $('html,body')
+                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                        return false;
+                    }
+                }
+            });
+
+            $(window).bind('scroll', function () {
+                var navHeight = $(window).height() -50;
+                if ($(window).scrollTop() > navHeight) {
+                    $('nav').addClass('fixed');
+                }
+                else {
+                    $('nav').removeClass('fixed');
+                }
+            });
+
+
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
